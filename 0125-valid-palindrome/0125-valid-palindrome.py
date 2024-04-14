d4ff3cecd6e6742e8fbdcb29f4d6440c39f4b02a
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-04-15 01:01:09 +0530

    Time: 50 ms (30.25%), Space: 17.7 MB (30.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,7 +4,7 @@ class Solution:
         s = s.lower()
         s = "".join(c for c in s if c.isalnum())
         for i in range(0,len(s)//2):
-            if s[i] != s[-i-1]:
+            if s[i] != s[len(s)-i-1]:
                 return False
         return True
                 
