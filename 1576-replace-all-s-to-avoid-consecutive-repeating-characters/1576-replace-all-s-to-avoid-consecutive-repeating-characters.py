53c76de34c5527b2d42fb0a59cd3bc0503fb4851
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-11 21:48:23 +0530

    Time: 31 ms (88.77%), Space: 16.6 MB (11.23%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -1,14 +1,19 @@
 class Solution:
     def modifyString(self, s: str) -> str:
         for i in range(len(s)):
-            if s[i] == "?" and i != len(s) - 1:
+            if s[i] == "?" and i != len(s) - 1 and i != 0:
                 j = 97
                 while ord(s[i-1]) == j or ord(s[i+1]) == j:
                     j += 1
                 s = s.replace("?", chr(j), 1)
-            elif s[i] == "?" and i == len(s) - 1:
+            elif i == len(s) - 1 and s[i] == "?" :
                 j = 97
                 while ord(s[i-1]) == j:
+                    j += 1
+                s = s.replace("?", chr(j), 1)
+            elif i == 0 and s[i] == "?" :
+                j = 97
+                while ord(s[i+1]) == j:
                     j += 1
                 s = s.replace("?", chr(j), 1)
         return s
