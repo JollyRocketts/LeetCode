5e61351bc45d8016e54b4a2fcc9059c135118676
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-16 04:55:43 +0530

    Time: 737 ms (53.69%), Space: 41.2 MB (54.94%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -7,16 +7,10 @@ class Solution:
             #idx = -1
             #idx -= 1
             while idx < len(group) - 1 and group[idx + 1][0] <= w:
-                '''if group[idx + 1][1] > m:
-                    m = group[idx + 1][1]
-                    s = idx + 1'''
                 heapq.heappush(hp, -group[idx + 1][1])
                 idx += 1
             if not hp:
                 break
-            #p = max(group[:idx+1], key = lambda x:x[1])
-            #w += p[1]
-            #group.remove(p)
             w -= heapq.heappop(hp)
         return w
     
