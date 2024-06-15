54540969a4dfc471635e9f5e5151f1f68f2553cb
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-16 04:56:12 +0530

    Time: 690 ms (82.35%), Space: 41.1 MB (73.72%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,13 +1,11 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
         group = sorted(zip(capital, profits))
-        idx = -1
+        idx = 0
         hp = []
         for i in range(k):
-            #idx = -1
-            #idx -= 1
-            while idx < len(group) - 1 and group[idx + 1][0] <= w:
-                heapq.heappush(hp, -group[idx + 1][1])
+            while idx < len(group) and group[idx][0] <= w:
+                heapq.heappush(hp, -group[idx][1])
                 idx += 1
             if not hp:
                 break
