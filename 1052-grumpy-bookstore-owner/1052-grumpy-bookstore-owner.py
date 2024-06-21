944523e04ca8f5233506fbe410c93fe4b17c1792
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-21 12:29:24 +0530

    Time: 4413 ms (5.38%), Space: 19.1 MB (14.93%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -5,7 +5,11 @@ class Solution:
         l = 0
         r = l + minutes
         while r<=len(grumpy):
+            if l>0 and z[l-1][0] > z[r-1][0] and z[l-1][1] == z[r-1][1] == 1:
+                l += 1
+                r += 1
+                continue
             m = max(m, sum([i[0] for i in z[l:r] if i[1] == 1]))
             l += 1
             r += 1
-        return sum([i[0] for i in z if i[1] == 0])+m
+        return sum([i[0] for i in z if i[1] == 0]) + m
