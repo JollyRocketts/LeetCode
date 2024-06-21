48119857b12e374550463092b5fcdcd71f924071
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-21 12:23:36 +0530

    Time: 4931 ms (5.38%), Space: 19.2 MB (5.21%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         z = [i for i in zip(customers,grumpy)]
-        res = sum([i[0] for i in z if i[1] == 0])
         m = 0
         l = 0
         r = l + minutes
@@ -9,4 +8,4 @@ class Solution:
             m = max(m, sum([i[0] for i in z[l:r] if i[1] == 1]))
             l += 1
             r += 1
-        return res+m
+        return sum([i[0] for i in z if i[1] == 0])+m
