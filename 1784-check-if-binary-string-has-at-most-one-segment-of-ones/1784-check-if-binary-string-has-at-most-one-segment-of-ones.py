9bb03f091cb480633d36ea7aeb8ed43982e10f1a
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-26 12:57:24 +0530

    Time: 34 ms (62.62%), Space: 16.4 MB (65.29%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,11 +1,21 @@
-from collections import Counter
+#from collections import Counter
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        c = Counter(s)['1']
-        for i in s:
-            if i == '1':
-                c -= 1
-            elif c > 0:
-                return False
-            if c == 0:
+        # c = Counter(s)['1']
+        # for i in s:
+        #     if i == '1':
+        #         c -= 1
+        #     elif c > 0:
+        #         return False
+        #     if c == 0:
+        #         return True
+        
+        i = 0
+        while s[i] == '1':
+            i += 1
+            if i == len(s):
                 return True
+        
+        if '1' in s[i:]:
+            return False
+        return True
