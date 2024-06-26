ea6e2b4ec702a03e3a0c77745efa207feea980f4
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-06-26 12:22:40 +0530

    Time: 156 ms (19.39%), Space: 18.1 MB (58.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,19 @@
-from collections import Counter
+#from collections import Counter
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        c = Counter(nums)[0]
-        for i in range(c):
-            nums.remove(0)
-            nums.append(0)
+        # for i in range(Counter(nums)[0]):
+        #     nums.remove(0)
+        #     nums.append(0)
+        i = 0
+        j = 0
+        
+        while j < len(nums) and i<len(nums):
+            if nums[i] == 0:
+                nums.append(0)
+                nums.pop(i)
+                j += 1
+            else:    
+                i += 1
