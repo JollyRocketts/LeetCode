9b94e3d80c689dcf286aad43f4bdf8cb089b9ad9
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-07-06 21:18:46 +0530

    Time: 34 ms (59.24%), Space: 16.6 MB (30.38%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,8 +1,6 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        if time//(n-1) == 0:
-            return time%n + 1
-        elif (time//(n-1))%2 == 0:
+        if (time//(n-1))%2 == 0:
                 return time%(n-1)+ 1
         else:
             return n-time%(n-1)
