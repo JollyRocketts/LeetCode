b2e929ad8ce770b42c52a54468ca2b1969016171
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-07-09 11:54:45 +0530

    Time: 33 ms (67.81%), Space: 16.5 MB (23.24%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -1,6 +1,6 @@
 class Solution:
     def reformatDate(self, date: str) -> str:
-        month = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
+        #month = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
         
         ans = ""
         
@@ -8,8 +8,36 @@ class Solution:
         
         ans += date[2]
         ans += "-"
-        ans += month[date[1]]
+        
+        #ans += month[date[1]]
+        
+        if date[1] == "Jan":
+            ans += "01"
+        elif date[1] == "Feb":
+            ans += "02"
+        elif date[1] == "Mar":
+            ans += "03"
+        elif date[1] == "Apr":
+            ans += "04"
+        elif date[1] == "May":
+            ans += "05"
+        elif date[1] == "Jun":
+            ans += "06"
+        elif date[1] == "Jul":
+            ans += "07"
+        elif date[1] == "Aug":
+            ans += "08"
+        elif date[1] == "Sep":
+            ans += "09"
+        elif date[1] == "Oct":
+            ans += "10"
+        elif date[1] == "Nov":
+            ans += "11"
+        elif date[1] == "Dec":
+            ans += "12"
+            
         ans += "-"
+        
         date[0] = date[0][:-2]
         if len(date[0]) == 1:
             ans += "0"
