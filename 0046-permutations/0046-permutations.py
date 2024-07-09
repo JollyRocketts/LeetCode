7dba75d46de5950e7c14ffe71e330744e29c512e
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-07-09 12:51:21 +0530

    Time: 30 ms (97.84%), Space: 16.8 MB (21.51%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,10 +1,27 @@
-from itertools import permutations
+#from itertools import permutations
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
         
-        for i in permutations(nums):
-            ans.append(i)
+#         for i in permutations(nums):
+#             ans.append(i)
+        
+        def addEle(a, l, r):
+            if l == r:
+                #ans.append(a)
+                #ans.extend(list(list(a)))
+                temp = []
+                temp.extend(a)
+                ans.append(temp)
+                #print(a)
+            else:
+                for i in range(l, r):
+                    a[l], a[i] = a[i], a[l]
+                    #print(a, l, r)
+                    addEle(a, l+1, r)
+                    a[l], a[i] = a[i], a[l]
+        
+        addEle(nums, 0, len(nums))
         
         return ans
