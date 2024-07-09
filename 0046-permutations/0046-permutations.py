5542dc71853c72ff2e2d10489e52a7eec56f77a7
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-07-09 12:19:54 +0530

    Time: 32 ms (94.74%), Space: 16.7 MB (21.51%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        
+        for i in permutations(nums):
+            ans.append(i)
+        
+        return ans
