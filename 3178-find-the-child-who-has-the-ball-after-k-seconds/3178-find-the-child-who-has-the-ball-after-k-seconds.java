1f639f79def31fb5255bfc197450dd1408258861
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-07-22 12:20:41 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (75.28%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        if((k/(n-1))%2 == 0) {
+            return(k%(n-1));
+        }
+        else {
+            return(n-1-k%(n-1));
+        }
+    }
+}
