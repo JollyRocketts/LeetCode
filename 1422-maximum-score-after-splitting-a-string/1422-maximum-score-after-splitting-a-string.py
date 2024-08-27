3e4b0ea3a1f19b7f1c415eefe6141e2e99caf1e4
JollyRocketts <121363057+JollyRocketts@users.noreply.github.com>
2024-08-28 03:15:35 +0530

    Time: 34 ms (81.76%), Space: 16.4 MB (96.72%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -1,23 +1,42 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         
-        def left(s):
-            score = 0
-            for i in s:
-                if i == "0":
-                    score += 1
-            return score
+        z = 0
+        o = 0
         
-        def right(s):
-            score = 0
-            for i in s:
-                if i == "1":
-                    score += 1
-            return score
+        ans = float('-inf')
         
-        ans = 0
+        for i in s:
+            if i == "1":
+                o += 1
         
-        for i in range(1, len(s)):
-            ans = max(ans, left(s[:i]) + right(s[i:]))
+        for i in s[:-1]:
+            if i == "0":
+                z += 1
+            else:
+                o -= 1
+                
+            ans = max(ans, z + o)
         
         return ans
+        
+#         def left(s):
+#             score = 0
+#             for i in s:
+#                 if i == "0":
+#                     score += 1
+#             return score
+        
+#         def right(s):
+#             score = 0
+#             for i in s:
+#                 if i == "1":
+#                     score += 1
+#             return score
+        
+#         ans = 0
+        
+#         for i in range(1, len(s)):
+#             ans = max(ans, left(s[:i]) + right(s[i:]))
+        
+#         return ans
